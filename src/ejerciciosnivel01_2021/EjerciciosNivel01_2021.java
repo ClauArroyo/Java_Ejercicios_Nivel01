42be11b961f7e4c05f1c441c07ec09cb26edcf0a
xp <xp@CETYSDAM00>
2021-01-21 13:24:00 +0100

    Primera parte del ejercicio realizada y comprobada que funciona. Nos queda por realizar la segunda parte del ejercicio.

--- src/ejerciciosnivel01_2021/EjerciciosNivel01_2021.java
+++ src/ejerciciosnivel01_2021/EjerciciosNivel01_2021.java
@@ -10,12 +10,31 @@ package ejerciciosnivel01_2021;
  * @author xp
  */
 public class EjerciciosNivel01_2021 {
-
+    
+    //declaro un array de integers para hacer pruebas
+    int[] listaNumeros = {50, 31, 27, 2, 5, 99};
+    
+    public int maximo(int[] lista){
+        int auxiliar = lista[0]; //el máximo va a ser el primer elemento del array 
+                                 //(lo vamos a suponer y luego lo compararemos con 
+                                 //el resto de los números de la lista
+        for (int i=0; i< lista.length; i++){
+            if (auxiliar < lista[i]){
+                auxiliar = lista[i];
+            }
+        }
+        return auxiliar;
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EjerciciosNivel01_2021 ejercicio = new EjerciciosNivel01_2021();
+        System.out.println("El máximo es: " + ejercicio.maximo(ejercicio.listaNumeros));
+        
+        
     }
     
 }
